b05984cb8d1c9ae6d859f32f2dc9a81f1acd3a58
mahesfsp <mahesinfotech2012@gmail.com>
2024-09-28 15:47:39 +0400

    fixed css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+
 
 export default function Header() {
     return (
         <header className="flex justify-between">
-            <Link to="/" className="text-lg font-bold">Maheswari Marimuthu</Link>
+          
             <ul className="flex">
-                <li className="nav-item"><Link to="/">Home</Link></li>
-                <li className="nav-item"><Link to="/about">About</Link></li>
-                <li className="nav-item"><Link to="/projects">Projects</Link></li>
-                <li className="nav-item"><Link to="/contact">Contact</Link></li>
+                <li className="nav-item"><a href="/">Home</a></li>
+                <li className="nav-item"><a href="/about">About</a></li>
+                <li className="nav-item"><a href="/projects">Projects</a></li>
+                <li className="nav-item"><a href="/contact">Contact</a></li>
             </ul>
         </header>
     );
